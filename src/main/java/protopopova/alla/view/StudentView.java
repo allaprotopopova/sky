40c427d1c38afe86e544f941f5570d833ea41c35
Alla Protopopova <a.v.protopopova@gmail.com>
2018-11-30 15:43:02 +0300

    left list changes work well, but not switch rendering

--- src/main/java/protopopova/alla/view/StudentView.java
+++ src/main/java/protopopova/alla/view/StudentView.java
@@ -19,74 +19,78 @@ import java.util.List;
 import java.util.Map;
 
 @Route(value = "student", layout = MyUI.class)
-@RouteAlias(value = "")
 public class StudentView extends HorizontalLayout {
 
     public static final String VIEW_NAME = "Student";
     private CollocationRepository repository = new CollocationRepositoryImpl();
+    private Button leftChecked;
+    private Button rightChecked;
 
     public StudentView() {
 
-        Map<Collocation, Button> mainWordList = new HashMap<>();
-        Map<Collocation, Button> pairWordList = new HashMap<>();
+//        Map<Collocation, Button> mainWordList = new HashMap<>();
+//        Map<Collocation, Button> pairWordList = new HashMap<>();
 
         List<Collocation> allCollocations = repository.getAll();
 
         Collections.shuffle(allCollocations);
         ListBox<Collocation> leftList = new ListBox<>();
+        Collections.shuffle(allCollocations);
+        ListBox<Collocation> rightList = new ListBox<>();
+
         leftList.setRenderer(new ComponentRenderer<>(coll -> {
             Button left = new Button(coll.getMainWord());
-            mainWordList.put(coll, left);
+            left.addClickListener(click -> {
+                    if (leftChecked != null) {
+                        leftChecked.removeThemeVariants(ButtonVariant.LUMO_SUCCESS, ButtonVariant.LUMO_ERROR);
+                    }
+                    leftChecked = left;
+                    Collocation leftCollValue = leftList.getValue();
+                    Collocation rightCollValue = rightList.getValue();
+                    if (rightCollValue == null) {
+                        left.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
+                    } else {
+                        rightChecked.removeThemeVariants(ButtonVariant.LUMO_SUCCESS, ButtonVariant.LUMO_ERROR);
+                        if (leftCollValue.getId() == rightCollValue.getId()) {
+                            left.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
+                            rightChecked.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
+
+                        } else {
+                            left.addThemeVariants(ButtonVariant.LUMO_ERROR);
+                            rightChecked.addThemeVariants(ButtonVariant.LUMO_ERROR);
+                            left.setEnabled(false);
+                            rightChecked.setEnabled(false);
+                            leftChecked = null;
+                            rightChecked = null;
+
+
+                        }
+                    }
+                }
+            );
             return left;
         }));
         leftList.setItems(allCollocations);
 
-        Collections.shuffle(allCollocations);
-        ListBox<Collocation> rightList = new ListBox<>();
+
         rightList.setRenderer(new ComponentRenderer<>(coll -> {
             Button right = new Button(coll.getPairWord());
-            pairWordList.put(coll, right);
+            right.addClickListener(click -> {
+                if (rightChecked != null) {
+                    rightChecked.removeThemeVariants(ButtonVariant.LUMO_SUCCESS, ButtonVariant.LUMO_ERROR);
+                }
+                rightChecked = right;
+
+            });
             return right;
         }));
         rightList.setItems(allCollocations);
 
-        leftList.addValueChangeListener(event -> {
-            if (event.getOldValue()!=null) {
-                Button oldButton = mainWordList.get(event.getOldValue());
-                oldButton.removeThemeVariants(ButtonVariant.LUMO_SUCCESS, ButtonVariant.LUMO_ERROR);
-            }
-            Collocation pairWord = rightList.getValue();
-            if (event.getValue()!=null) {
-                Button valueButton = mainWordList.get(event.getValue());
-                if (pairWord==null) {
-                    valueButton.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
-                } else {
-                    Button pairButton = pairWordList.get(pairWord);
-                    if (event.getValue().getId()==pairWord.getId()) {
-                        valueButton.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
-                        pairButton.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
-
-                    } else {
-                        valueButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
-                        pairButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
-                    }
-                }
-            }
-
-        });
-
-        rightList.addValueChangeListener(event -> {
-            if (event.getOldValue()!=null) {
-                Button oldButton = pairWordList.get(event.getOldValue());
-                oldButton.removeThemeVariants(ButtonVariant.LUMO_SUCCESS, ButtonVariant.LUMO_ERROR);
-            }
-        });
 
         add(leftList);
         add(rightList);
 
 
-
-        }
+    }
 
 }
--- src/main/java/protopopova/alla/view/TeacherView.java
+++ src/main/java/protopopova/alla/view/TeacherView.java
@@ -1,13 +1,32 @@
 package protopopova.alla.view;
 
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.listbox.ListBox;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.router.RoutePrefix;
 import protopopova.alla.MyUI;
 
 @Route(value = "teacher", layout = MyUI.class)
-public class TeacherView extends FlexLayout {
+@RouteAlias(value = "")
+public class TeacherView extends HorizontalLayout {
 
     public static final String VIEW_NAME = "Teacher";
+
+    public TeacherView() {
+        ListBox<String> listBox = new ListBox<>();
+        listBox.setItems("Bread", "Butter", "Milk");
+
+// Adding components to the end:
+        listBox.add(new H3("After all the items"));
+
+// Adding components after a specific item:
+        listBox.addComponents("Butter", new H3("After butter"));
+
+// Adding components before a specific item:
+        listBox.prependComponents("Bread", new H3("Before bread"));
+        add(listBox);
+    }
 }
