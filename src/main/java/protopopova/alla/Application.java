45fda2fecfc6b5b84eebf0c96bb1c9227cdf785f
Alla Protopopova <a.v.protopopova@gmail.com>
2018-12-26 17:26:05 +0300

    add spring boot but not data-jpa

--- src/main/java/protopopova/alla/Application.java
+++ src/main/java/protopopova/alla/Application.java
@@ -1,24 +1,40 @@
 package protopopova.alla;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinServlet;
+import com.vaadin.flow.spring.SpringServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import protopopova.alla.model.Collocation;
 import protopopova.alla.repository.CollocationRepository;
+import protopopova.alla.repository.CollocationRepositoryImpl;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) {
-        log.debug("enter main");
-        SpringApplication.run(Application.class);
-        log.debug("exit main");
+    @Bean
+    @Scope("prototype")
+    public UI ui() {
+        return new MyUI();
+    }
+
+    @Bean
+    public CollocationRepository collocationRepository() {
+        return new CollocationRepositoryImpl();
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/protopopova/alla/MyUI.java
+++ src/main/java/protopopova/alla/MyUI.java
@@ -1,6 +1,7 @@
 package protopopova.alla;
 
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
@@ -8,27 +9,31 @@ import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RoutePrefix;
 import com.vaadin.flow.router.RouterLayout;
+import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import protopopova.alla.view.StudentView;
 import protopopova.alla.view.TeacherView;
 
-
+@Push
+@SpringComponent
 @StyleSheet("css/shared-styles.css")
 @Theme(value = Lumo.class, variant = Lumo.LIGHT)
-public class MyUI extends FlexLayout implements RouterLayout {
+public class MyUI extends UI implements RouterLayout {
 
     private Menu menu;
 
     public MyUI() {
-        setSizeFull();
-        setClassName("main-layout");
+        FlexLayout flex = new FlexLayout();
+        flex.setSizeFull();
+        flex.setClassName("main-layout");
 
         menu = new Menu();
         menu.addView(TeacherView.class, TeacherView.VIEW_NAME, VaadinIcon.ACADEMY_CAP.create());
         menu.addView(StudentView.class, StudentView.VIEW_NAME, VaadinIcon.EDIT.create());
-        add(menu);
+        flex.add(menu);
+        add(flex);
     }
 
 }
