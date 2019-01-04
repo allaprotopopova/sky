a88e54f10b46009be62a9ca02d707b0953336b1a
Alla Protopopova <a.v.protopopova@gmail.com>
2019-01-05 00:10:09 +0300

    Add Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:    java $JAVA_OPTS -DCLIFF_ROOT="." -jar target/dependency/webapp-runner.jar --port $PORT target/*.war
--- hr.sh
+++ hr.sh
@@ -1,2 +1,2 @@
 mvn -B -s settings.xml -DskipTests=true clean package
-java -DDATABASE_URL="postgres://user:password@localhost:5432/cliff" -jar target/dependency/webapp-runner.jar target/*.war
+java -DDATABASE_URL="postgres://ztkchfmkudwrmx:9091e0c28fdc1bc89776a15800e4aa35ab5cc12dec862de2f836211d6c369073@ec2-174-129-18-247.compute-1.amazonaws.com:5432/d7eh52uaenodl4" -jar target/dependency/webapp-runner.jar target/*.war
